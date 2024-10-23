c246d1561bd8bc3b40ecf00a742ac0b930561e45
Mr-Sim <simoncatanese.pro@gmail.com>
2024-10-23 13:03:14 +0200

    begin minmax

--- ExercicesCpp/Morpion MinMax/MinMax.h
+++ ExercicesCpp/Morpion MinMax/MinMax.h
@@ -1,5 +1,24 @@
 #pragma once
+#include "Grid.h"
+#include <vector>
+#include <optional>
+
 class MinMax
 {
+	MinMaxTree root;
 };
 
+class MinMaxTree {
+private:
+	std::vector<TreeNode> childs;
+};
+
+
+class TreeNode {
+public:
+	int getScore();
+private:
+	std::optional<int> score;
+	Grid configuration;
+	std::vector<TreeNode> childs;
+};
--- ExercicesCpp/Morpion MinMax/Morpion MinMax.cpp
+++ ExercicesCpp/Morpion MinMax/Morpion MinMax.cpp
@@ -16,22 +16,25 @@ int main()
         int y;
         int numpad;
         std::wcout << L"choisir votre tour :";
-        std::cin >> numpad;
+        
+        do {
+            std::cin >> numpad;
 
-        switch (numpad) {
-        case 1: x = 2; y = 0;break;
-        case 2: x = 2; y = 1;break;
-        case 3: x = 2; y = 2;break;
-        case 4: x = 1; y = 0;break;
-        case 5: x = 1; y = 1;break;
-        case 6: x = 1; y = 2;break;
-        case 7: x = 0; y = 0;break;
-        case 8: x = 0; y = 1;break;
-        case 9: x = 0; y = 2;break;
-        }
+            switch (numpad) {
+            case 1: x = 2; y = 0;break;
+            case 2: x = 2; y = 1;break;
+            case 3: x = 2; y = 2;break;
+            case 4: x = 1; y = 0;break;
+            case 5: x = 1; y = 1;break;
+            case 6: x = 1; y = 2;break;
+            case 7: x = 0; y = 0;break;
+            case 8: x = 0; y = 1;break;
+            case 9: x = 0; y = 2;break;
+            }
+        } while (!morpion.playTurn(nextTurn, x, y));
+        
 
 
-        morpion.playTurn(nextTurn, x, y);
         nextTurn = (nextTurn == EnumTileState::Cross) ? EnumTileState::Circle : EnumTileState::Cross;
         morpion.printGrid();
     }
